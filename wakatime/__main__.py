484de6c93d806f583380779655a705c65f1cb8d5
Jeremiah Boby <mail@jeremiahboby.me>
2020-07-11 16:58:25 +0100

    Use HTML table for README

--- wakatime/__main__.py
+++ wakatime/__main__.py
@@ -8,5 +8,26 @@ from wakatime import USER
 
 stats = json.load(
     request.urlopen(f"https://wakatime.com/api/v1/users/{USER}/stats/last_7_days")
+)["data"]
+
+languages = sorted(
+    stats["languages"],
+    reverse=True,
+    key=lambda language: language["total_seconds"]
 )
-print(stats["languages"])
+
+
+def make_row(language: dict) -> str:
+    """Make a HTML table row for the provided language."""
+    return f"<tr><td>{ language['name'] }</td><td>{ language['text'] }</td></tr>"
+
+
+print(
+    f"""
+    # Languages I use
+
+    <table>
+          { ''.join(make_row(language) for language in languages) }
+    </table>
+    """
+)
